f854bd95eb02e333464f97a7c501b842af8474c6
Oğuz Han Tarhan <36084657+oguzhantarhan@users.noreply.github.com>
2025-06-29 18:18:58 +0300

    Update page.tsx

--- src/app/(dashboard)/(private)/[id]/cards/page.tsx
+++ src/app/(dashboard)/(private)/[id]/cards/page.tsx
@@ -3,15 +3,14 @@ import CardsView from '@/views/pages/Cards'
 import { Grid } from '@mui/material'
 import React from 'react'
 
-const Card = async({ params }: { params: { id: number } }) => {
-  const { id } = await params
-  const data=await GetCardsByColelctionId(id)
+const Card = async ({ params }: { params: { id: number } }) => {
+  const { id } = params
+  const data = await GetCardsByColelctionId(id)
+
   return (
-<Grid container justifyContent={"center"} width={"100vw"} height={"100vh"}>
-
- <CardsView  CardData={data}></CardsView>
-
-</Grid>
+    <Grid container justifyContent={"center"} width={"100vw"} height={"100vh"}>
+      <CardsView CardData={data} />
+    </Grid>
   )
 }
 
