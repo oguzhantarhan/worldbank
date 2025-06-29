c3164692510f2526157f1ea97d68f988f74f21ce
Oğuz Han Tarhan <36084657+oguzhantarhan@users.noreply.github.com>
2025-06-29 18:49:59 +0300

    Update page.tsx

--- src/app/(dashboard)/(private)/[id]/cards/page.tsx
+++ src/app/(dashboard)/(private)/[id]/cards/page.tsx
@@ -2,9 +2,8 @@ import { GetCardsByColelctionId } from '@/Services/CardServices'
 import CardsView from '@/views/pages/Cards'
 import { Grid } from '@mui/material'
 
-export default async function Page({ params }: { params: Record<string, string> }) {
-  const id = Number(params.id)
-  const data = await GetCardsByColelctionId(id)
+export default async function Page({ params }: { params: { id: string } }) {
+  const data = await GetCardsByColelctionId(Number(params.id))
 
   return (
     <Grid container justifyContent="center" width="100vw" height="100vh">
