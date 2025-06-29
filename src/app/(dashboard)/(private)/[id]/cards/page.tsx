b3c60872d821adb4230b4d12892129f1326798eb
Oğuz Han Tarhan <36084657+oguzhantarhan@users.noreply.github.com>
2025-06-29 18:30:25 +0300

    Update page.tsx

--- src/app/(dashboard)/(private)/[id]/cards/page.tsx
+++ src/app/(dashboard)/(private)/[id]/cards/page.tsx
@@ -1,23 +1,14 @@
 import { GetCardsByColelctionId } from '@/Services/CardServices'
 import CardsView from '@/views/pages/Cards'
 import { Grid } from '@mui/material'
-import React from 'react'
 
-type Props = {
-  params: {
-    id: string
-  }
-}
-
-const Card = async ({ params }: Props) => {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id)
   const data = await GetCardsByColelctionId(id)
 
   return (
-    <Grid container justifyContent={"center"} width={"100vw"} height={"100vh"}>
+    <Grid container justifyContent="center" width="100vw" height="100vh">
       <CardsView CardData={data} />
     </Grid>
   )
 }
-
-export default Card
