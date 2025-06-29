c3bf2e386b37bee6496f03142d6b9c722545342d
Oğuz Han Tarhan <36084657+oguzhantarhan@users.noreply.github.com>
2025-06-29 19:45:46 +0300

    Update page.tsx

--- src/app/(dashboard)/(private)/[id]/cards/page.tsx
+++ src/app/(dashboard)/(private)/[id]/cards/page.tsx
@@ -1,23 +1,20 @@
-// pages/cards/[id].tsx
+// app/(dashboard)/(private)/[id]/cards/page.tsx
+
 import { GetCardsByColelctionId } from '@/Services/CardServices'
 import CardsView from '@/views/pages/Cards'
 import { Grid } from '@mui/material'
-import { GetServerSideProps, NextPage } from 'next'
 
-type Props = {
-  cardData: Awaited<ReturnType<typeof GetCardsByColelctionId>>
+export default async function Page({
+  params
+}: {
+  params: { id: string }
+}) {
+  // Sunucu tarafında çalışır, doğrudan await kullanabilirsin
+  const data = await GetCardsByColelctionId(Number(params.id))
+
+  return (
+    <Grid container justifyContent="center" width="100vw" height="100vh">
+      <CardsView CardData={data} />
+    </Grid>
+  )
 }
-
-const CardPage: NextPage<Props> = ({ cardData }) => (
-  <Grid container justifyContent="center" width="100vw" height="100vh">
-    <CardsView CardData={cardData} />
-  </Grid>
-)
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({ params }) => {
-  const id = Number(params?.id)
-  const data = await GetCardsByColelctionId(id)
-  return { props: { cardData: data } }
-}
-
-export default CardPage
